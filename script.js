0b661a0fddc2ceb9b7a0c68554fc86da1499305f
Eric Arbour <arboureric@gmail.com>
2017-05-01 18:10:33 -0400

    fix bugs

--- script.js
+++ script.js
@@ -3,8 +3,12 @@ var app = angular.module('myModule', []);
 app.controller('myController', function($scope) {
   $scope.itemArray = ['Study', 'Eat Peanut Butter', 'Watch Seinfeld'];
   $scope.addItem = function(item) {
+    if ($scope.item === "" || $scope.item === undefined) {
+      return;
+    }
     var newItem = item;
     $scope.itemArray.push(newItem);
+    $scope.item = "";
   }
   $scope.removeItem = function(i) {
     $scope.itemArray.splice(i, 1);
